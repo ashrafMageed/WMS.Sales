2eb5001d4f774dd8ea36884f783a6279f99ae781
ashrafMageed <ashraf_mageed@hotmail.com>
2014-05-19 22:30:10 +0100

    Initial Commit

---
+++ src/SalesService/Sales.Endpoint/PurchaseModule.cs
@@ -0,0 +1,26 @@
+﻿using Nancy;
+using Nancy.ModelBinding;
+using Raven.Client.Document;
+
+namespace Sales.Endpoint
+{
+    public class PurchaseModule : NancyModule
+    {
+        public PurchaseModule()
+        {
+            Post["/Purchase"] = parameters =>
+                {
+                    var purchaseOrder = this.Bind<PurchaseOrder>();
+                    var store = new DocumentStore { Url = "http://localhost:8080", DefaultDatabase = "SalesDB" }.Initialize();
+                    using (var session = store.OpenSession())
+                    {
+                        session.Store(purchaseOrder);
+                        session.SaveChanges();
+                    }
+
+                    return "Purchased";
+                }; 
+        }
+    }
+
+}
---
+++ src/SalesService/Sales.Endpoint/PurchaseOrder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sales.Endpoint
+{
+    public class PurchaseOrder
+    {
+        public Guid OrderId { get; set; }
+        public List<Guid> ProductIds { get; set; }
+    }
+}
